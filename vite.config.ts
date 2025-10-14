66d88611cb6f3566b2b52c9468c2de6f97b1427d
igroman4ik-pc <ni4000009@gmail.com>
2025-10-15 02:11:17 +0300

    chore: add husky, lint-staged, commitizen, cz-conventional-changelog, babel-plugin-react-compiler
    update dependencies | update tsconfigs | add .gitattributes,
    @ant-design/v5-patch-for-react-19 for compability react 19, husky pre-commit, prepare-commit-msg
    hooks
    
    BREAKING CHANGE: update dependencies and devDependencies (npm i)

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
 	const isProduction = mode === 'production';
 
 	return {
-		plugins: [react(), tailwindcss()],
+		plugins: [
+			react({
+				babel: {
+					plugins: [['babel-plugin-react-compiler']],
+				},
+			}),
+			tailwindcss(),
+		],
 		base: isProduction ? '/' : '/',
 		build: {
 			outDir: 'dist',
